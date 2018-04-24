a407a4f58d6a9009e970dd0afd2731c28124a6c7
Gowtham <Gowtham_D02@edgeverve.com>
2018-04-24 16:39:09 +0530

    Patch for paper-dialog modal attribute, Minor upgrade to tab-form templates

--- oe-studio-utils.js
+++ oe-studio-utils.js
@@ -12,3 +12,29 @@ window.OEUtils._getRestApiUrl = function(path){
     var restApiRoot = (window.OEUtils && window.OEUtils.restApiRoot) ? window.OEUtils.restApiRoot : '/api';
     return restApiRoot + path;
 }
+
+
+//Fix for nested dialog box with modal property
+window.addEventListener('iron-overlay-opened',function(e){
+    //handle backdrop;
+    var dialog = e.target;
+    if(dialog.tagName.toLowerCase() !== 'paper-dialog'){
+        return;
+    }
+    var parentEle = dialog.parentNode;
+    var overlay = document.querySelector('iron-overlay-backdrop');
+    if(overlay){
+        parentEle.insertBefore(overlay,dialog);
+    }
+});
+window.addEventListener('iron-overlay-closed',function(e){
+    //handle backdrop
+    var dialog = e.target;
+    if(dialog.tagName.toLowerCase() !== 'paper-dialog'){
+        return;
+    }
+    var overlay = document.querySelector('iron-overlay-backdrop');
+    if(overlay){
+        overlay.parentNode.removeChild(overlay);
+    }
+})
